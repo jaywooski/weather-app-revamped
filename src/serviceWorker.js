82f92a14ff8c0a6c5af2655c87d8777ee823443c
John Wooley <john.m.wooley@gmail.com>
2023-04-06 23:11:26 -0500

    Commented out files and imports to allow build warning-free

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const CACHE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
 
 // this.addEventListener("install", (e) => {
 // 	e.waitUntil(
